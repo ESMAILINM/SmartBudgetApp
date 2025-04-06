b35ce843e5fb503c513a5c82c81721527100197f
ESMAILINM <esmailinmartinez05@gmail.com>
2025-04-06 15:35:03 -0400

    Crea modelo Categorias

---
+++ SmartBudgetApp/Models/Categorias.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartBudgetApp.Models
+{
+    public class Categorias
+    {
+        [Key]
+        public int CategoriaId { get; set; }
+
+        [Required(ErrorMessage = "El nombre de la categoría es obligatorio")]
+        [StringLength(50, ErrorMessage = "El nombre no puede tener más de 50 caracteres")]
+        public string Nombre { get; set; }
+
+    }
+
+   
+}
