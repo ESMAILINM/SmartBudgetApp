1f18e692afd15753203f3be0cfc646ce38b544e6
ESMAILINM <esmailinmartinez05@gmail.com>
2025-04-06 15:35:48 -0400

    Crea modelo Gastos

---
+++ SmartBudgetApp/Models/Gastos.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace SmartBudgetApp.Models
+{
+    public class Gastos
+    {
+        [Key]
+        public int GastoId { get; set; }
+
+        [Required(ErrorMessage = "La descripción es obligatoria.")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El monto es obligatorio.")]
+        [Range(0, double.MaxValue, ErrorMessage = "El monto debe ser positivo.")]
+        public decimal Monto { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "La categoría es obligatoria.")]
+        [ForeignKey("Categorias")]
+        public int CategoriaId { get; set; }
+
+        public Categorias? Categorias { get; set; }
+    }
+}
