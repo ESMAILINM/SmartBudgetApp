62c26c11be38ea057c92b0990e750b3f0927bd9c
ESMAILINM <esmailinmartinez05@gmail.com>
2025-04-06 15:35:34 -0400

    Crea modelo Facturas

---
+++ SmartBudgetApp/Models/Facturas.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartBudgetApp.Models
+{
+    public class Facturas
+    {
+        [Key]
+        public int FacturaId { get; set; }
+
+        [Required(ErrorMessage = "La categoría es obligatoria.")]
+        public int CategoriaId { get; set; } 
+
+        public Categorias Categoria { get; set; } 
+
+        [Required(ErrorMessage = "Estado es obligatorio")]
+        public string Estado { get; set; }
+
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Total es obligatorio")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El total debe ser mayor a cero")]
+        public decimal Total { get; set; }
+    }
+}
