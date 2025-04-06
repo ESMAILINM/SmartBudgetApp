f3e22e7212cf325015d72a39257ad5116ae25d27
ESMAILINM <esmailinmartinez05@gmail.com>
2025-04-06 15:35:58 -0400

    Crea modelo Ingresos

---
+++ SmartBudgetApp/Models/Ingresos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartBudgetApp.Models
+{
+    public class Ingresos
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Descripcion { get; set; } = string.Empty;
+        public decimal Monto { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+    }
+}
