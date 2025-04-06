635f23397c41f45a8b4562ee4df9180ee4d682b4
ESMAILINM <esmailinmartinez05@gmail.com>
2025-04-06 15:35:18 -0400

    Crea modelo Departamentos

---
+++ SmartBudgetApp/Models/Departamento.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartBudgetApp.Models
+{
+    public class Departamento
+    {
+        [Key]
+        public int DepartamentoId { get; set; }
+
+        [Required(ErrorMessage = "El nombre del departamento es obligatorio")]
+        [StringLength(50, ErrorMessage = "El nombre no puede tener más de 50 caracteres")]
+        public string Nombre { get; set; }
+    }
+}
