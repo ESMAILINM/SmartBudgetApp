b21f853fbd4e42ea0b6b29c3c289c42b42026b36
ESMAILINM <esmailinmartinez05@gmail.com>
2025-04-06 15:45:34 -0400

    Agrega estilo para elementos chats de la pagina home

---
+++ SmartBudgetApp/wwwroot/lib/bootstrap/dist/js/chart-config.js
@@ -0,0 +1,22 @@
+﻿function renderBarChart(chartId, chartData) {
+    var ctx = document.getElementById(chartId).getContext('2d');
+    new Chart(ctx, {
+        type: 'bar',
+        data: chartData,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+function renderPieChart(chartId, chartData) {
+    var ctx = document.getElementById(chartId).getContext('2d');
+    new Chart(ctx, {
+        type: 'pie',
+        data: chartData
+    });
+}
