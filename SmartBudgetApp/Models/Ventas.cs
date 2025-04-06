6eb16b98b5dd3d53767a7e752331dd7dfd862b7b
ESMAILINM <esmailinmartinez05@gmail.com>
2025-04-06 15:36:35 -0400

    Crea modelo Ventas

---
+++ SmartBudgetApp/Models/Ventas.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartBudgetApp.Models
+{
+    public class Ventas
+    {
+        [Key]
+        public int VentaId { get; set; }
+        [Required(ErrorMessage = "El departamento es obligatorio.")]
+
+        [ForeignKey("DepartamentoId")]
+        public int DepartamentoId { get; set; }
+        public Departamento? Departamento { get; set; }
+
+        [Required(ErrorMessage = "El total es obligatorio.")]
+        [Range(0, double.MaxValue, ErrorMessage = "El total debe ser un valor positivo.")]
+        public decimal Total { get; set; }
+
+        public DateTime Fecha { get; set; }
+    }
+}
+
